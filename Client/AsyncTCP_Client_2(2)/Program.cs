2a9fb551a2d31c75fad71ad4853a3f4a269dde42
Danil <1bl@ukr.net>
2016-11-21 18:39:36 +0200

    init commit

---
+++ Client/AsyncTCP_Client_2(2)/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace AsyncTCP_Client_2_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPAddress ipAddr = IPAddress.Parse("127.0.0.1");
+            int host = 11000;
+            IPEndPoint ipEnd = new IPEndPoint(ipAddr, host);
+            Console.Write("Enter your name: ");
+            string UserName = Console.ReadLine();
+
+            Socket sender = new Socket(ipAddr.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            string send = null;
+
+            try
+            {
+                sender.Connect(ipEnd);
+                sender.Send(Encoding.UTF8.GetBytes(UserName));
+
+                Task.Factory.StartNew((() =>
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            byte[] bytes = new byte[1024];
+                            int recieve = sender.Receive(bytes);
+                            string rec = Encoding.UTF8.GetString(bytes, 0, recieve);
+                            if (rec != send)
+                                Console.WriteLine(rec);
+                        }
+                        catch (Exception)
+                        {
+
+                        }
+                    }
+                }));
+
+                while (true)
+                {
+                    send = Console.ReadLine();
+                    if (send.ToLower() == "exit")
+                    {
+                        sender.Send(Encoding.UTF8.GetBytes("Exit"));
+                        sender.Send(Encoding.UTF8.GetBytes(UserName));
+                        sender.Shutdown(SocketShutdown.Both);
+                        sender.Close();
+                        break;
+                    }
+
+                    send = UserName + ": " + send;
+                    sender.Send(Encoding.UTF8.GetBytes(send));
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
---
+++ Server/AsyncTCP_Server_2/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace AsyncTCP_Server_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int port = 11000;
+            IPAddress ipAddr = IPAddress.Parse("127.0.0.1");
+            IPEndPoint ipEnd = new IPEndPoint(ipAddr,port);
+
+            Dictionary<string,Socket> dic = new Dictionary<string, Socket>();
+
+            Socket sListener = new Socket(ipAddr.AddressFamily,SocketType.Stream,ProtocolType.Tcp);
+
+            List<Socket> sockets = new List<Socket>(); 
+            try
+            {
+                sListener.Bind(ipEnd);
+                sListener.Listen(20);
+
+                while (true)
+                {
+                    byte[] user = new byte[1024];
+                    Socket accept = sListener.Accept();
+                    
+
+                    int UserRec = accept.Receive(user);
+                    string Name = Encoding.UTF8.GetString(user, 0, UserRec);
+
+                    dic[Name] = accept;
+                    Console.WriteLine("К нам подключился " + Name);
+
+                    Task.Factory.StartNew((() =>
+                    {
+                        while (true)
+                        {
+                            string str = "Server: "+Console.ReadLine();
+
+                            
+                        }
+                    }));
+
+
+                    Task.Factory.StartNew((() =>
+                    {
+                        try
+                        {
+                            Socket acceptTask = accept;
+
+                            while (true)
+                            {
+                                byte[] bytes = new byte[1024];
+
+                                string str = null;
+
+                                int recieve = acceptTask.Receive(bytes);
+
+                                string rec = Encoding.UTF8.GetString(bytes, 0, recieve);
+
+                                if (rec == "Exit")
+                                {
+                                    recieve = acceptTask.Receive(bytes);
+                                    Console.WriteLine(Encoding.UTF8.GetString(bytes, 0, recieve)+" покинул нас.");
+                                    dic.Remove(Encoding.UTF8.GetString(bytes, 0, recieve));
+                                    break;
+                                }
+
+                                Console.WriteLine(rec);
+
+                                foreach (var socket in sockets)
+                                {
+                                    socket.Send(Encoding.UTF8.GetBytes(rec));
+                                }
+                                
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            Console.WriteLine("Он покинул нас...");
+                            throw;
+                        }
+                    }));
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+        }
+    }
+}
